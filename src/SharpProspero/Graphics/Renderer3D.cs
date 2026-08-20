65eae941941098bca290d9c46de633e2d8671794
U-DESKTOP-DP5I6U0\Hjemme-gamer <david@live.dk>
2026-08-20 22:44:01 +0200

    Call sceVideoOutSubmitFlip explicitly after AgcDevice.SuspendPoint

--- src/SharpProspero/Graphics/Renderer3D.cs
+++ src/SharpProspero/Graphics/Renderer3D.cs
@@ -285,8 +285,9 @@ public sealed unsafe class Renderer3D : IDisposable
         if (_firstDraw && trace) _trace?.Invoke("AGC_STAGE_SUSPEND_OK");
 
         if (_firstDraw && trace) _trace?.Invoke("AGC_STAGE_FLIP_BEGIN frame=" + _display.FrameIndex);
+        int flipRc = VideoOut.sceVideoOutSubmitFlip(_display.OutputHandle, _display.CurrentBufferIndex, VideoOutFlipModeVSync, _display.FrameIndex);
         _display.AdvanceFrame();
-        if (_firstDraw && trace) _trace?.Invoke("AGC_STAGE_FLIP_OK");
+        if (_firstDraw && trace) _trace?.Invoke("AGC_STAGE_FLIP_OK rc=0x" + flipRc.ToString("X8"));
         _firstDraw = false;
         _slot = (_slot + 1) % _framesInFlight;
     }
